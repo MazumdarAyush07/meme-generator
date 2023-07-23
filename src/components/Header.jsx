9767d4a92a8d8fef0ad2f0eccb76c05e51a594b5
MazumdarAyush07 <mazumdarayush.2002@gmail.com>
2023-07-23 16:49:02 +0530

    created nav

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { ReactDOM } from "react";
+import trollFace from "../../public/assets/TrollFace.png";
+
+function Header() {
+  return (
+    <nav className="bg-slate-900 m-0 flex justify-between bg-gradient-to-r from-violet-800 to-violet-600 text-white">
+      <div className="flex p-4 items-center">
+        <img src={trollFace} alt="" className="h-8 w-10" />
+        <h2 className="p-4">Meme Generator</h2>
+      </div>
+      <div className="p-8">
+        <h2 className="font-sans">React - Project 3</h2>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
